8d2e16756d307218b2878ac4fa253d63e75b05dd
mrOrlando <juriy88@mail.com>
2017-08-05 17:25:36 +0300

    step 2. create first app.

---
+++ README.md
@@ -0,0 +1,6 @@
+
+Уроки по Webpack 2
+https://youtu.be/X6qde-zvw00?list=PLbZerpEHZ8s0GgEcddz186-xRs8X0t0rA
+
+yarn install
+yarn run start
---
+++ source/index.js
@@ -0,0 +1,9 @@
+import createMenu from './menu';
+
+let menu = createMenu([
+  'Главная',
+  'Обо мне',
+  'Портфолио'
+], 'menu');
+
+document.body.appendChild(menu);

d9e768eaf9c8c1d86b3051fd904b0a2fe48fd95b
mrOrlando <juriy88@mail.com>
2017-08-05 20:18:35 +0300

    step 6. Separated on production and development.

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const PATHS = {
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
   entry: {
     'index': PATHS.source + '/pages/index/index.js',
     'blog': PATHS.source + '/pages/blog/blog.js',
@@ -35,8 +35,25 @@ module.exports = {
           pretty: true
         }
     }]
-  },
-  devServer: {
-    stats: 'errors-only'
   }
 };
+
+const developmentConfig = {
+  devServer: {
+    stats: 'errors-only',
+    port: 9000
+  }
+}
+
+module.exports = function (env) {
+  if (env === 'production') {
+    return common;
+  }
+  if (env === 'development') {
+    return Object.assign(
+      {},
+      common,
+      developmentConfig
+    );
+  }
+}

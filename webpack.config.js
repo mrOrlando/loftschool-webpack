0757b0687ae9ef6b0c5a7f3ebfe03b71e9fb8381
mrOrlando <juriy88@mail.com>
2017-08-05 17:55:16 +0300

    step 4. Add pug and pug-loader.

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,16 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Webpack app'
+      template: PATHS.source + '/index.pug'
     })
-  ]
+  ],
+  module: {
+    rules: [{
+        test: /\.pug$/,
+        loader: 'pug-loader',
+        options: {
+          pretty: true
+        }
+    }]
+  }
 };
